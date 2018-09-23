019b60dc8caefd923540b7628a3ab65618ee21d4
Rob Konsdorf <robertkonsdorf@gmail.com>
2018-09-23 06:42:48 -0400

    Add demo blaclist contract

---
+++ eosio_docker/contracts/blacklisted.cpp
@@ -0,0 +1,20 @@
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/print.hpp>
+#include "famileos.cpp"
+
+using namespace eosio;
+
+class blacklisted : public eosio::contract {
+ public:
+     using contract::contract;
+
+     /// @abi action
+     void thing(account_name child) {
+     	action(
+     		permission_level{ _self, N(active) },
+     		N(famileosiopc),
+     		N(validate),
+     		std::make_tuple(child, _self, N(thing))
+     	).send();
+     }
+};
